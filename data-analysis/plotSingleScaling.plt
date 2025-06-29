b947510c79f756df1199f48616a82d9dd38109c6
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-06-29 13:37:54 +0200

    Plot all single scaling graphs

--- data-analysis/plotSingleScaling.plt
+++ data-analysis/plotSingleScaling.plt
@@ -3,7 +3,8 @@
 set encoding iso_8859_1
 set terminal pdf size 5,10
 
-set out 'singleScaling.pdf'
+outname = sprintf("scaling_%s.pdf", model)
+set out outname
 
 set multiplot layout 4,1
 
@@ -11,7 +12,7 @@ set y2tics
 set y2range [0:15]
 
 do for [i in "1 10 20 40"] {
-    filename = sprintf("plottable_%s.csv", i)
+    filename = sprintf("plottable_%s_%s.csv", i, model)
     set title i
     plot filename using ($0*5):1 with linespoints title 'Pods' axis x1y2, \
          filename using ($0*5):2 with linespoints title 'Average TTFT', \
--- data-analysis/plotSingleScaling.sh
+++ data-analysis/plotSingleScaling.sh
@@ -1,12 +1,32 @@
+#!/bin/bash
 
-for targetRequests in 2 5 10
+set -e
+
+mkdir -p results
+
+for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
 do
-	for parallel in 1 10 20 40
+	for targetRequests in 2 5 10
 	do
-		cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_ollama-llama3-3-70b_"$targetRequests".csv \
+		for parallel in 1 10 20 40
+		do
+			cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_ollama-llama3-3-70b_"$targetRequests".csv \
+				| awk '{sum += $5; sum2 += $6; count++; if (count == 5) { print $3" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
+				> results/plottable_"$parallel"_"$model".csv
+		done
+		cd results
+		gnuplot -e "model='$model'" ../plotSingleScaling.plt
+		mv "scaling_$model.pdf" "scaling_$model"_"$targetRequests.pdf"
+		cd ..
+	done
+done
+
+export targetRequests=5
+export parallel=20
+
+cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_ollama-llama3-3-70b_"$targetRequests".csv \
 			| awk '{sum += $5; sum2 += $6; count++; if (count == 5) { print $3" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
 			> plottable_"$parallel".csv
-	done
-	./plotSingleScaling.plt
-	mv singleScaling.pdf singleScaling_targetRequests"$targetRequests".pdf
-done
+./plotExampleScaling.plt
+
+mv exampleScaling.pdf results/
