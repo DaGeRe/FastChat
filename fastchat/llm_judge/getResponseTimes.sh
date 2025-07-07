196be24e9b74c00213196062a305c0b59d9a5364
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-07-07 19:27:40 +0200

    Fix getResponseTimes

--- fastchat/llm_judge/getResponseTimes.sh
+++ fastchat/llm_judge/getResponseTimes.sh
@@ -5,12 +5,17 @@ do
 	echo "== Analyzing with targetRequests=$targetRequests"
 	for parallelity in 40 20 10 1
 	do
-		for model in ollama-codestral-22b ollama-deepseek-coder-33b ollama-deepseek-r1-70b ollama-llama3-3-70b ollama-starcoder2-15b vllm-deepseek-coder-33b-instruct-2gpus vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-nvidia-llama-3-3-70b-instruct-fp8 vllm-systran-faster-whisper-medium-en
+		for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
 		do
 			kubectl patch model $model --type merge -p '{"spec": {"targetRequests": '$targetRequests'}}'
 			echo "== Getting answers for model $model $parallelity == "
 			python gen_api_answer.py --model $model --openai-api-base https://kiara.sc.uni-leipzig.de/api --parallel $parallelity
 			mv data/responseTime.csv data/parallel_$parallelity"_"$model"_"$targetRequests.csv
+			echo "Resetting replicas"
+                        kubectl patch model $model --type merge -p '{"spec": {"maxReplicas": 1}}'
+                        sleep 30s
+                        kubectl patch model $model --type merge -p '{"spec": {"maxReplicas": 64}}'
+
 			echo
 			echo
 		done
