0cc75551b5ea8f4d36400dc9cafe37b94e538cd7
DaGeRe <davidgeorg_reichelt@dagere.de>
2025-07-09 10:43:00 +0200

    Adapt to vllm analysis

--- data-analysis/plotSingleScaling.plt
+++ data-analysis/plotSingleScaling.plt
@@ -1,19 +1,20 @@
 #!/bin/gnuplot
 
 set encoding iso_8859_1
-set terminal pdf size 5,10
+set terminal pdf size 5,15
 
 outname = sprintf("scaling_%s.pdf", model)
 set out outname
 
-set multiplot layout 4,1
+set multiplot layout 5,1
 
 set y2tics
 set y2range [0:15]
 
-do for [i in "1 10 20 40"] {
-    filename = sprintf("plottable_%s_%s.csv", i, model)
-    set title i
+do for [parallelity in "1 10 20 40 100"] {
+    filename = sprintf("plottable_%s_%s.csv", parallelity, model)
+    currentTitle = sprintf("parallelity=%s, targetRequests=%d", parallelity, targetRequests);
+    set title currentTitle
     plot filename using ($0*5):1 with linespoints title 'Pods' axis x1y2, \
          filename using ($0*5):2 with linespoints title 'Average TTFT', \
          filename using ($0*5):3 with linespoints title 'Average TPS'
--- data-analysis/plotSingleScaling.sh
+++ data-analysis/plotSingleScaling.sh
@@ -6,25 +6,25 @@ mkdir -p results
 
 for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
 do
-	for targetRequests in 2 5 10
+	for targetRequests in 10 20
 	do
-		for parallel in 1 10 20 40
+		for parallel in 100 40 20 10 1
 		do
-			cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_ollama-llama3-3-70b_"$targetRequests".csv \
+			cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_vllm-llama-4-scout-17b-16e-instruct_"$targetRequests".csv \
 				| awk '{sum += $5; sum2 += $6; count++; if (count == 5) { print $3" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
 				> results/plottable_"$parallel"_"$model".csv
 		done
 		cd results
-		gnuplot -e "model='$model'" ../plotSingleScaling.plt
+		gnuplot -e "model='$model'" -e "targetRequests=$targetRequests" ../plotSingleScaling.plt
 		mv "scaling_$model.pdf" "scaling_$model"_"$targetRequests.pdf"
 		cd ..
 	done
 done
 
-export targetRequests=5
-export parallel=20
+export targetRequests=10
+export parallel=100
 
-cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_ollama-llama3-3-70b_"$targetRequests".csv \
+cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_vllm-llama-4-scout-17b-16e-instruct_"$targetRequests".csv \
 			| awk '{sum += $5; sum2 += $6; count++; if (count == 5) { print $3" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
 			> plottable_"$parallel".csv
 ./plotExampleScaling.plt
