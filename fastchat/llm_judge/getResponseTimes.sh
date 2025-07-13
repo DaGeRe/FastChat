a7df14ff5e7cfdf836e6001ed35db26c7b584adf
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-07-13 11:21:29 +0200

    Execute model by model, fix model_answer moving

--- fastchat/llm_judge/getResponseTimes.sh
+++ fastchat/llm_judge/getResponseTimes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for targetRequests in 5 10 20
+for model in vllm-mistral-small-24b-instruct-2501 vllm-meta-llama-llama-3-3-70b-instruct vllm-llama-4-scout-17b-16e-instruct ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-nvidia-llama-3-3-70b-instruct-fp8
 do
 	echo "== Analyzing with targetRequests=$targetRequests"
 	for parallelity in 100 50 20 10 1
 	do
-		for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
+		for targetRequests in 5 10 20
 		do
 			kubectl patch model $model --type merge -p '{"spec": {"targetRequests": '$targetRequests'}}'
 			if [ "$parallelity" -eq 1 ]; then
@@ -41,8 +41,8 @@ do
 		echo "Moving everything to $resultFolder"
 		mkdir $resultFolder
 		mv data/*csv $resultFolder
-		mv data/mt_bench/model_answer/ $resultFolder
-		mkdir data/mt_bench/model_answer/
+		mkdir $resultFolder/model_answer/
+		mv data/mt_bench/model_answer/* $resultFolder/model_answer/
 	done
 done
 
