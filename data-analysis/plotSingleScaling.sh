d90c224a817b438dff625e1fa4024c1229ea7851
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-07-15 10:32:25 +0200

    Sort result folders

--- data-analysis/plotSingleScaling.sh
+++ data-analysis/plotSingleScaling.sh
@@ -6,17 +6,18 @@ mkdir -p results
 
 for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
 do
+	mkdir -p results/$model
 	for targetRequests in 5 10 20
 	do
 		for parallel in 100 50 20 10 1
 		do
 			cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_"$model"_"$targetRequests".csv \
-				| awk '{sum += $8; sum2 += $9; count++; if (count == 5) { print $4" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
+				| awk '{sum += $8; sum2 += $9; count++; if (count == 5) { print $6" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
 				> results/plottable_"$parallel"_"$model".csv
 		done
 		cd results
 		gnuplot -e "model='$model'" -e "targetRequests=$targetRequests" ../plotSingleScaling.plt
-		mv "scaling_$model.pdf" "scaling_$model"_"$targetRequests.pdf"
+		mv "scaling_$model.pdf" "$model/scaling_$model"_"$targetRequests.pdf"
 		cd ..
 	done
 done
