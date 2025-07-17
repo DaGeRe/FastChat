efc4e3ae957574966e266df5b82665c3c6ffc432
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-07-17 15:44:29 +0200

    Make scaling plot scripts compatible with new csv structure

--- data-analysis/plotSingleScaling.plt
+++ data-analysis/plotSingleScaling.plt
@@ -1,10 +1,12 @@
 #!/bin/gnuplot
 
 set encoding iso_8859_1
-set terminal pdf size 5,15
+set terminal pdf size 5,10
 
 outname = sprintf("scaling_%s.pdf", model)
 set out outname
+
+set key top left
 
 set multiplot layout 5,1
 
@@ -13,11 +15,13 @@ set y2range [0:15]
 
 do for [parallelity in "1 10 20 50 100"] {
     filename = sprintf("plottable_%s_%s.csv", parallelity, model)
+ #   print "Filename: " .filename
     currentTitle = sprintf("parallelity=%s, targetRequests=%d", parallelity, targetRequests);
     set title currentTitle
-    plot filename using ($0*5):1 with linespoints title 'Pods' axis x1y2, \
-         filename using ($0*5):2 with linespoints title 'Average TTFT', \
-         filename using ($0*5):3 with linespoints title 'Average TPS'
+    plot filename using ($0*5):3 with linespoints title 'Pods' axis x1y2, \
+         filename using ($0*5):4 with linespoints title 'Average TTFT', \
+         filename using ($0*5):5 with linespoints title 'Average TPS'
+
 }
 	
 unset multiplot
--- data-analysis/plotSingleScaling.sh
+++ data-analysis/plotSingleScaling.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+function getSum {
+  awk -vOFMT=%.10g '{sum += $1; square += $1^2} END {print sqrt(square / NR - (sum/NR)^2)" "sum/NR" "NR}'
+}
+
+
 set -e
 
 mkdir -p results
 
-for model in ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8
+for model in vllm-mistral-small-24b-instruct-2501 vllm-meta-llama-llama-3-3-70b-instruct vllm-llama-4-scout-17b-16e-instruct
 do
+	echo $model
 	mkdir -p results/$model
 	for targetRequests in 5 10 20
 	do
 		for parallel in 100 50 20 10 1
 		do
-			cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_"$model"_"$targetRequests".csv \
-				| awk '{sum += $8; sum2 += $9; count++; if (count == 5) { print $6" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
-				> results/plottable_"$parallel"_"$model".csv
+			file="target_"$targetRequests"_"$parallel"/parallel_"$parallel"_"$model"_"$targetRequests".csv"
+			if [ -f $file ]
+			then
+				cat target_"$targetRequests"_"$parallel"/parallel_"$parallel"_"$model"_"$targetRequests".csv \
+					| awk '{sum += $8; sum2 += $9; count++; if (count == 5) { print $4" "$5" "$6" "sum / 5" "sum2/5; sum = 0; sum2=0; count = 0; } }' \
+					> results/plottable_"$parallel"_"$model".csv
+				echo -n $targetRequests" "$parallel" "
+				cat results/plottable_"$parallel"_"$model".csv | head -n 100 | awk '{print $5}' | getSum
+			fi
 		done
 		cd results
+		
+		
 		gnuplot -e "model='$model'" -e "targetRequests=$targetRequests" ../plotSingleScaling.plt
 		mv "scaling_$model.pdf" "$model/scaling_$model"_"$targetRequests.pdf"
 		cd ..
-	done
+	done > results/scaling_$model.csv
 done
+
+paste -d " " results/scaling_vllm-mistral-small-24b-instruct-2501.csv results/scaling_vllm-meta-llama-llama-3-3-70b-instruct.csv results/scaling_vllm-llama-4-scout-17b-16e-instruct.csv | grep "^5 "> results/scaling_all.csv
+
+exit 1
 
 export targetRequests=10
 export parallel=100
