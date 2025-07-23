4847c3b0214ef3c09fad099058a6da9cbc1117c4
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2025-07-23 14:27:19 +0200

    Update judgement to new configuration

--- fastchat/llm_judge/getJudgements.sh
+++ fastchat/llm_judge/getJudgements.sh
@@ -1,13 +1,13 @@
-for targetRequests in 2 5 10
+for targetRequests in 5 10 20
 do
 	echo "== Analyzing with targetRequests=$targetRequests"
-	for parallelity in 40 20 10 1
+	for parallelity in 100 50 20 10 1
 	do
 		resultFolder="data/target_"$targetRequests"_"$parallelity
 		echo "Taking data from $resultFolder"
 		cp -R $resultFolder/model_answer data/mt_bench/
 		python gen_judgment.py --mode single \
-			--model-list ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-llama-4-scout-17b-16e-instruct vllm-meta-llama-llama-3-3-70b-instruct vllm-mistral-small-24b-instruct-2501 vllm-nvidia-llama-3-3-70b-instruct-fp8 \
+			--model-list vllm-mistral-small-24b-instruct-2501 vllm-meta-llama-llama-3-3-70b-instruct vllm-llama-4-scout-17b-16e-instruct ollama-llama3-3-70b vllm-deepseek-coder-33b-instruct vllm-deepseek-r1-distill-llama-70b vllm-llama-3-3-nemotron-super-49b-v1 vllm-nvidia-llama-3-3-70b-instruct-fp8 \
 			--parallel 10 \
 			--openai-api-base https://kiara.sc.uni-leipzig.de/api \
 			--judge-model vllm-llama-4-scout-17b-16e-instruct
